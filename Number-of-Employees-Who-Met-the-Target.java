63fa6c75ab09bdb23982c483f5aca602eeb240d8
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-03-23 11:01:15 +0530

    [2798] [Time Beats: 100.00%] [Memory Beats: 54.34%] - LeetPush

---
+++ Number-of-Employees-Who-Met-the-Target.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+3        int count=0;
+4        for(int i=0;i<hours.length;i++){
+5            if(hours[i]>=target){
+6                count++;
+7            }
+8        }
+9        return count;
+10        
+11    }
+12}
