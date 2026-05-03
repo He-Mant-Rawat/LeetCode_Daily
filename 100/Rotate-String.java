e492d0eca996d14daddbcef08b6e8f472d0cade3
Hemant Rawat <hemantrawat20062004hr@gmail.com>
2026-05-03 17:58:37 +0530

    [796] [Time Beats: 100.00%] [Memory Beats: 42.97%] - LeetPush

---
+++ 100/Rotate-String.java
@@ -0,0 +1,12 @@
+1class Solution {
+2    public boolean rotateString(String s, String goal) {
+3             if(s.length()!=goal.length()){
+4            return false;
+5        }   
+6        if((s+s).contains(goal)){
+7            return true;
+8        }  
+9        return false;   
+10    
+11    }
+12}
